46990b27febecf44bd1fc86717a4d3db8c4d103e
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-04-26 15:51:15 +0900

    :+1: 競プロ用g++エイリアスの名前を変更した

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -10,7 +10,7 @@ end
 ############################################################################
 ##                                  Alias                                 ##
 ############################################################################
-alias vim "nvim"
+alias vi "nvim"
 alias exa "exa --icons"
 alias ls "exa"
 alias cat "bat"
@@ -19,7 +19,7 @@ alias find "fd"
 alias ps "procs"
 alias grep "rg"
 alias comproide "tmux source-file ~/Documents/tmux-layout"
-alias g++ "g++ -Wall -Wextra -pedantic -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector"
+alias gpp "g++ -Wall -Wextra -pedantic -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector"
 ############################################################################
 
 
